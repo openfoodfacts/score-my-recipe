cf2c66199821fe1515c3065c611128366cd54928
Alex Garel <alex@garel.org>
2026-06-16 12:29:25 +0200

    feat: API to parse recipe from text

--- server/api/api.py
+++ server/api/api.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import api.recipes as recipes
+import api.types as types
 
 app = FastAPI(
     title="Score My Recipe",
@@ -15,3 +17,12 @@ async def root() -> dict:
 @app.get("/v1/health")
 async def health() -> dict:
     return {"status": "ok"}
+
+# TODO: use a class for parameters to constraint values of lang
+@app.post("/v1/parse_text")
+async def parse_text(text: str, lang: str) -> types.RecipeParseResponse:
+    """Parse a text and return a list of ingredients with quantities and eventual modifiers
+    """
+    ingredients = await recipes.parse_text(text, lang)
+    return types.RecipeParseResponse(ingredients=ingredients)
+
---
+++ server/api/types.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class OFFIngredient(BaseModel):
+    """Ingredient model for Open Food Facts API"""
+    # TODO convert str to bool and is_in_taxonomy to bool
+    id: str
+    text: str
+    quantity: Optional[str] = None
+    quantity_g: Optional[float] = None
+    ecobalyse_code: Optional[str] = None
+    ciqual_food_code: Optional[str] = None
+    is_in_taxonomy: int = None
+
+
+class RecipeIngredient(BaseModel):
+    """Ingredient model for Score My Recipe API"""
+    taxonomy_id: Optional[str]  = None
+    is_in_taxonomy: bool
+    codified_ingredient: str
+    quantity_g: Optional[float] = None
+
+
+class RecipeParseResponse(BaseModel):
+    """Response model for parse_text endpoint"""
+    ingredients: list[RecipeIngredient]
